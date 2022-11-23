892742ce16b814fac7f5f4108f80deb7171ac414
dimazivan <dimazivan740@gmail.com>
2022-11-24 05:24:14 +0700

    add unfinished fiture and some error on it v2

---
+++ app/Http/Requests/UserStoreRequest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // return false;
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        if (request()->isMethod('post')) {
+            return[
+                'first_name' => 'required|max:255',
+                'last_name' => 'required|max:255',
+                'username' => 'required|min:5|max:255',
+                'email' => 'required|email',
+                'password' => 'required|min:5|max:40',
+                'cbrole' => 'required',
+                'phone' => 'required|numeric|digits_between:10,13',
+                'address' => 'required|max:255',
+                'desc' => 'max:255',
+                'file_foto' => 'mimes:jpeg,jpg,png,gif|max:10000',
+            ];
+        } else {
+            return[
+                'first_name' => 'required|max:255',
+                'last_name' => 'required|max:255',
+                'username' => 'required|min:5|max:255',
+                'email' => 'required|email',
+                'password' => 'required|min:5|max:40',
+                'cbrole' => 'required',
+                'phone' => 'required|numeric|digits_between:10,13',
+                'address' => 'required|max:255',
+                'desc' => 'max:255',
+                'file_foto' => 'mimes:jpeg,jpg,png,gif|max:10000',
+            ];
+        }
+    }
+
+    public function messages()
+    {
+        if (request()->isMethod('post')) {
+            return[
+                'first_name.required' => 'Nama Depan Silahkan Diisi',
+                'last_name.required' => 'Nama Belakang Silahkan Diisi',
+                'username.required' => 'Username Silahkan Diisi',
+                'email.required' => 'Email Silahkan Diisi',
+                'password.required' => 'PAssword Silahkan Diisi',
+                'cbrole.required' => 'Role User Silahkan Dipilih',
+                'phone.required' => 'Nomor Telepon Silahkan Diisi',
+                'address.required' => 'Alamat Silahkan Diisi',
+                'desc.required' => 'Detail Alamat Silahkan Diisi',
+                'file_foto.required' => 'Silahkan Mengupload File Foto',
+            ];
+        } else {
+            return[
+                'first_name.required' => 'Nama Depan Silahkan Diisi',
+                'last_name.required' => 'Nama Belakang Silahkan Diisi',
+                'username.required' => 'Username Silahkan Diisi',
+                'email.required' => 'Email Silahkan Diisi',
+                'password.required' => 'PAssword Silahkan Diisi',
+                'cbrole.required' => 'Role User Silahkan Dipilih',
+                'phone.required' => 'Nomor Telepon Silahkan Diisi',
+                'address.required' => 'Alamat Silahkan Diisi',
+                'desc.required' => 'Detail Alamat Silahkan Diisi',
+                'file_foto.required' => 'Silahkan Mengupload File Foto',
+            ];
+        }
+    }
+}
