4c5c0af2b2aa4d7825edf51af0979e584bc38815
Dimaz Ivan Perdana <dimazivan740@gmail.com>
2023-05-21 18:16:27 +0700

    change auth api to laravel sanctum

---
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserStoreRequest;
+use App\Models\Roles;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthApiController extends Controller
+{
+    public function register(Request $request)
+    // public function register(UserStoreRequest $request)
+    {
+        $waktu = Carbon::now();
+
+        // $validator = $request->validated();
+
+        // if (!$validator) {
+        //     // if($validator->fails()) {
+        //     return response()->json($validator->errors());
+        // }
+
+        // Cek Data
+        $cek = User::cekUsername($request->username)
+        ->cekEmail($request->email)
+        ->get();
+
+        $cek_role = Roles::where('id', $request->cbrole)->get();
+
+        if (empty($cek[0])) {
+            if ($request->hasFile('file_foto')) {
+                if (filesize($request->file_foto) > 1000 * 10000) {
+                    //failed save to database
+                    return response()->json([
+                        'success' => false,
+                        'message' => 'Fotone Kegedean mas e',
+                    ], 409);
+                }
+
+                if ($request->file_foto->getClientOriginalExtension() == "jpg" ||
+                $request->file_foto->getClientOriginalExtension() == "jpeg" ||
+                $request->file_foto->getClientOriginalExtension() == "png" ||
+                $request->file_foto->getClientOriginalExtension() == "gif") {
+                    if ($request->hasFile('file_foto')) {
+                        $file = $request->file('file_foto');
+                        $nama_file = time() . "_" . $file->getClientOriginalName();
+                        $file->storeAs('/data/image/user/', $nama_file);
+                    } else {
+                        $nama_file = "";
+                    }
+
+                    // Query insert dengan foto
+                    $user = User::create([
+                        'first_name' => $request->first_name,
+                        'last_name' => $request->last_name,
+                        'username' => $request->username,
+                        'email' => $request->email,
+                        'roles_id' => $cek_role[0]->id,
+                        'role' => $cek_role[0]->name,
+                        'status' => 2,
+                        'country' => $request->cbcountry,
+                        'province' => $request->cbprovince,
+                        'city' => $request->cbcity,
+                        'district' => $request->cbdistrict,
+                        'ward' => $request->cbward,
+                        'phone' => $request->phone,
+                        'address' => $request->address,
+                        'detail_address' => $request->desc,
+                        'password' => bcrypt($request->password),
+                        'file_foto' => $nama_file,
+                        'created_at' => $waktu,
+                        'updated_at' => $waktu,
+                    ]);
+                } else {
+                    //failed save to database
+                    return response()->json([
+                        'success' => false,
+                        'message' => 'Format file foto e di cek maneh mas e',
+                    ], 409);
+                }
+            } else {
+                // Query insert tanpa foto
+                $user = User::create([
+                    'first_name' => $request->first_name,
+                    'last_name' => $request->last_name,
+                    'username' => $request->username,
+                    'email' => $request->email,
+                    'roles_id' => $cek_role[0]->id,
+                    'role' => $cek_role[0]->name,
+                    'status' => 2,
+                    'country' => $request->cbcountry,
+                    'province' => $request->cbprovince,
+                    'city' => $request->cbcity,
+                    'district' => $request->cbdistrict,
+                    'ward' => $request->cbward,
+                    'phone' => $request->phone,
+                    'address' => $request->address,
+                    'detail_address' => $request->desc,
+                    'password' => bcrypt($request->password),
+                    'created_at' => $waktu,
+                    'updated_at' => $waktu,
+                ]);
+            }
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Data Kembar wes kesimpen mas e',
+            ], 409);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()
+            ->json([
+                'data' => $user,
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+            ]);
+    }
+
+    public function login(Request $request)
+    {
+        // if (!Auth::attempt($request->only('email', 'password'))) {
+        if (!Auth::attempt($request->only('username', 'password'))) {
+            return response()
+                ->json(['message' => 'Unauthorized'], 401);
+        }
+
+        // if(Auth::check()) {
+        //     return response()->json([
+        //         'success' => false,
+        //         'message' => 'Wes login ya login maneh',
+        //     ], 401);
+        // }
+
+        // $user = User::where('email', $request['email'])->firstOrFail();
+        $user = User::where('users.username', $request['username'])
+        ->join('roles', 'roles.id', 'users.roles_id')
+        ->firstOrFail();
+
+        // dd(
+        //     $user,
+        // );
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()
+            ->json([
+                'message' => 'Hi '.$user->first_name.', welcome to home',
+                'role' => $user->name,
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+            ]);
+    }
+
+    public function cek_login()
+    {
+        if(Auth::check()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Wes login ya login maneh',
+            ], 401);
+        } else {
+            return response()->json([
+                'success' => true,
+                'message' => 'Kosong mas gas',
+            ], 200);
+        }
+    }
+
+    // method for user logout and delete token
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        auth()->guard('web')->logout();
+
+        return [
+            'message' => 'You have successfully logged out and the token was successfully deleted'
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Log\Log_UserController;
 use App\Http\Controllers\Log\Log_AuthController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Api\Api_WilayahController;
+use App\Http\Controllers\Api\AuthApiController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Component\CaptchaController;
 use App\Models\Product;
@@ -24,21 +25,46 @@ use RealRashid\SweetAlert\Facades\Alert;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-// URL API AUTH
-Route::group([
-    'namespace' => 'App\Http\Controllers\Api',
-    'middleware' => ['auth','CekRole:admin,keuangan']
-], function () {
+//API route for register new user
+Route::post('/register', [AuthApiController::class, 'register']);
+//API route for login user
+Route::post('/login', [AuthApiController::class, 'login']);
+
+Route::get('/cek_login', [AuthApiController::class, 'cek_login']);
+
+//Protecting Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+
+    // API route for logout user
+    Route::post('/logout', [AuthApiController::class, 'logout']);
+
     // Api User
     Route::resource('api_user', 'Api_UserController');
 
     // Get Cb Wilayah Indonesia
     Route::resource('api_wilayah', 'Api_WilayahController');
 });
+
+
+
+// URL API AUTH
+// Route::group([
+//     'namespace' => 'App\Http\Controllers\Api',
+//     'middleware' => ['auth','CekRole:admin,keuangan']
+// ], function () {
+//     // Api User
+//     Route::resource('api_user', 'Api_UserController');
+
+//     // Get Cb Wilayah Indonesia
+//     Route::resource('api_wilayah', 'Api_WilayahController');
+// });
 
 
 // URL API PUBLIC
