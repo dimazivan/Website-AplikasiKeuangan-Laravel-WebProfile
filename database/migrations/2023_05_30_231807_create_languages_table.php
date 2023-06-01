68f01f20482f151af5d48d16525e9d25f0742716
Dimaz Ivan Perdana <dimazivan740@gmail.com>
2023-06-01 12:01:12 +0700

    ahay

--- database/migrations/2023_05_30_231807_create_languages_table.php
+++ database/migrations/2023_05_30_231807_create_languages_table.php
@@ -15,7 +15,7 @@ return new class () extends Migration {
         Schema::create('languages', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('alias');
+            $table->string('alias')->nullable();
             $table->timestamps();
         });
     }
