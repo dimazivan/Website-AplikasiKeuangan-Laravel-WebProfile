cd8a919e272aa2a821bd2cc095cc1d27fb5b1d26
Dimaz Ivan Perdana <dimazivan740@gmail.com>
2023-06-04 15:00:40 +0700

    add pricing page

---
+++ app/Http/Controllers/Landing/PricingController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\Landing;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PricingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $data = "a";
+
+        // dd(
+        //     $data,
+        //     $data2,
+        //     Route::current(),
+        //     Route::currentRouteName(),
+        //     $host = $_SERVER['SERVER_NAME'],
+        //     url()->current(),
+        //     $part = explode('/', url()->current()),
+        //     request()->segments(),
+        //     request()->segment(1),
+        // );
+
+        return view('landing.pages.pricing.pricing', [
+            'data' => $data,
+        ]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
